92bc9bc5f361ff42b5c6606d94beae4552b4bd3a
tattkh <tatianakhoury@live.com.au>
2022-05-08 21:42:40 +1000

    finished title and characters

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,4 +1,4 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 function Create() {
@@ -6,7 +6,7 @@ function Create() {
         title: '',
         genre: '',
         tone: '',
-        characters: [],
+        characters: '',
         length: 0,
         iterations: 0
     });
@@ -26,28 +26,76 @@ function Create() {
 
     return (
         <>
-            <FormControl fullWidth>
-                <InputLabel id='genre-select-label'>Genre</InputLabel>
-                <Select
-                    labelId='genre-select-label'
-                    id='genre-select'
-                    value={state.genre}
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="flex-start"
+                spacing={2}
+            >
+                <TextField
+                    required
+                    id='title-input'
+                    label='Title'
+                    name='title'
+                    value={state.title}
                     onChange={handleChange}
-                    label='Genre'
-                    name='genre'
+                />
+                <FormControl fullWidth>
+                    <InputLabel id='genre-select-label'>Genre</InputLabel>
+                    <Select
+                        labelId='genre-select-label'
+                        id='genre-select'
+                        value={state.genre}
+                        onChange={handleChange}
+                        label='Genre'
+                        name='genre'
+                        required
 
-                >
-                    <MenuItem value="action">Action</MenuItem>
-                    <MenuItem value="comedy">Comedy</MenuItem>
-                    <MenuItem value="drama">Drama</MenuItem>
-                    <MenuItem value="fantasy">Fantasy</MenuItem>
-                    <MenuItem value="horror">Horror</MenuItem>
-                    <MenuItem value="mystery">Mystery</MenuItem>
-                    <MenuItem value="romance">Romance</MenuItem>
-                    <MenuItem value="thriller">Thriller</MenuItem>
-                    <MenuItem value="western">Western</MenuItem>
-                </Select>
-            </FormControl> 
+                    >
+                        <MenuItem value="action">Action</MenuItem>
+                        <MenuItem value="comedy">Comedy</MenuItem>
+                        <MenuItem value="drama">Drama</MenuItem>
+                        <MenuItem value="fantasy">Fantasy</MenuItem>
+                        <MenuItem value="horror">Horror</MenuItem>
+                        <MenuItem value="mystery">Mystery</MenuItem>
+                        <MenuItem value="romance">Romance</MenuItem>
+                        <MenuItem value="thriller">Thriller</MenuItem>
+                        <MenuItem value="western">Western</MenuItem>
+                    </Select>
+                </FormControl> 
+                <FormControl fullWidth>
+                    <InputLabel id='tone-select-label'>Tone</InputLabel>
+                    <Select
+                        labelId='tone-select-label'
+                        id='tone-select'
+                        value={state.tone}
+                        onChange={handleChange}
+                        label='Tone'
+                        name='tone'
+                        required
+
+                    >
+                        <MenuItem value="happy">Happy</MenuItem>
+                        <MenuItem value="sad">Sad</MenuItem>
+                        <MenuItem value="serious">Serious</MenuItem>
+                        <MenuItem value="humorous">Humorous</MenuItem>
+                        <MenuItem value="threatening">Threatening</MenuItem>
+                        <MenuItem value="pessimistic">Pessimistic</MenuItem>
+                        <MenuItem value="optimistic">Optimistic</MenuItem>
+                        <MenuItem value="anxious">Anxious</MenuItem>
+                        <MenuItem value="depressing">Depressing</MenuItem>
+                        <MenuItem value="excited">Excited</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField
+                    required
+                    id='characters-input'
+                    label='Characters'
+                    name='characters'
+                    value={state.characters}
+                    onChange={handleChange}
+                />
+            </Stack>
         </>
     );
 }
